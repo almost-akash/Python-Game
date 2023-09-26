81e0ea30376789824452ffc697dd5cde3b932393
Almost <asahu54007@gmail.com>
2023-09-27 00:03:29 +0530

    Added BG and Player

--- Game.py
+++ Game.py
@@ -2,19 +2,30 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 500 , 400
+WIDTH, HEIGHT = 800 , 700
 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Demo Game")
+BG = pygame.transform.scale(pygame.image.load("bg.jpg"),(WIDTH,HEIGHT))
+PLAYER_WIDTH, PLAYER_HEIGHT = 30,50
 
+def draw(player):
+    WIN.blit(BG,(0,0))
+    pygame.draw.rect(WIN,"red",player)
+    pygame.display.update()
+    
 def main():
     run = True
+    
+    player = pygame.Rect(200,HEIGHT - PLAYER_HEIGHT,PLAYER_WIDTH,PLAYER_HEIGHT)
     
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+            
+        draw(player)
             
     pygame.quit()
     
